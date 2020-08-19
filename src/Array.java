6e5cb909d6adf309602b70471afb9417229b8258
prarthna <prarthna.sahu@gmail.com>
2020-08-19 13:14:29 +0100

    added

---
+++ src/Array.java
@@ -0,0 +1,16 @@
+
+public class Array {
+	
+	public static void main (String[] args){
+		
+		int[] a = new int[5];
+		a[0]=8; a[1]=10; a[2]=33; a[3]=66; a[4]=22;
+		
+		//ot
+		
+		int[] b = {13,16,18,23,28,99};
+		
+		System.out.println(a[2] + " " + b[3]);
+	}
+
+}
